09092bfb1ce371736410a8efbf5d5d9eed7e8084
Franco Martín Salgado Sierra <frco.indie@gmail.com>
2024-10-24 12:58:56 -0300

    Rework del escarabajo y mis mapas
    Rework del script del escarabajo (falta agregarle los cambios de Rodrigo), se agregó la flag on_ground a la bola y su implicancia en el elefante

--- scripts/dung_ball.gd
+++ scripts/dung_ball.gd
@@ -3,12 +3,15 @@ extends RigidBody2D
 
 signal grow_dung_ball
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite
+@onready var hit_box: CollisionShape2D = $HitBox
 
 @export var dung_n: int = 0
 
+var on_ground: bool = false
 
+
+# --- Functions ---
 func _ready() -> void:
 	dung_size(dung_n)
 
@@ -20,6 +23,16 @@ func _process(delta: float) -> void:
 # To manage the size and weight of the dung ball
 func dung_size(n: int):
 	mass += n/2
-	sprite_2d.frame = n
-	collision_shape_2d.shape.radius = 5 + n
+	sprite.frame = n
+	hit_box.shape.radius = 5 + n
 	dung_n += 1
+
+
+# Ground Detection
+func _on_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Ground"):
+		on_ground = true
+
+func _on_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Ground"):
+		on_ground = false
