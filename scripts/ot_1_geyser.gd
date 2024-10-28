c71d3c05185d47431cb89a9e08febfcc4b0140f6
Rodrigo Alarcón Milla <hamster22.ok@gmail.com>
2024-10-27 23:21:54 -0300

    Alfa
    alfa

--- scripts/ot_1_geyser.gd
+++ scripts/ot_1_geyser.gd
@@ -3,13 +3,17 @@ extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var BASE_FORCE_VECTOR: Vector2 = Vector2(0.0, 100.0)
+@export var DUNG_FORCE_VECTOR: Vector2 = Vector2(0.0, 100.0)
+@export var BEETLE_FORCE_VECTOR: Vector2 = Vector2(0.0, 50.0)
+
+
 @export_subgroup("Bodies")
 @export var beetle: CharacterBody2D
 @export var dung_ball: RigidBody2D
 
 var dung_close: bool
-
+var beetle_close: bool
+var rng = RandomNumberGenerator.new()
 
 func _process(delta: float) -> void:
 	eject()
@@ -17,16 +21,24 @@ func _process(delta: float) -> void:
 
 func eject() -> void:
 	if dung_close && animated_sprite_2d.frame == 3:
-		dung_ball.linear_velocity -= BASE_FORCE_VECTOR
+		DUNG_FORCE_VECTOR.x = rng.randf_range(-5.0, 5.0) 
+		dung_ball.linear_velocity -= DUNG_FORCE_VECTOR
+	if beetle_close && animated_sprite_2d.frame == 3:
+		BEETLE_FORCE_VECTOR.x = rng.randf_range(-5.0, 5.0) 
+		beetle.velocity -= BEETLE_FORCE_VECTOR
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == dung_ball:
 		dung_close = true
+	if body == beetle:
+		beetle_close = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body == dung_ball:
 		dung_close = false
+	if body == beetle:
+		beetle_close = false
 
 
 func _on_timer_timeout() -> void:
