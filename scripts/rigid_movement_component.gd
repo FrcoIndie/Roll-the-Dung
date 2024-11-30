151bbcf0fe2afe0af756c98e581d8822e1e95746
Franco Martín Salgado Sierra <frco.indie@gmail.com>
2024-11-29 21:15:16 -0300

    RigidBody slopes

--- scripts/rigid_movement_component.gd
+++ scripts/rigid_movement_component.gd
@@ -2,7 +2,7 @@ class_name RigidMovementComponent
 extends Node2D
 
 
-const IMPULSE: float = 0.25
+const IMPULSE: float = 1.0
 const MAX_SPEED: float = 200.0
 
 var velocity = Vector2.ZERO
@@ -10,7 +10,12 @@ var velocity = Vector2.ZERO
 
 func beetle_movement(body: RigidBody2D, direction: float, delta: float):
 	if body.on_floor || body.over_dung:
-		velocity.x = direction * IMPULSE
+		var slope_angle = body.slope_angle # Vector // a la pendiente
+		var slope_direction = Vector2(cos(slope_angle), -sin(slope_angle)).normalized()
+		
+		velocity = slope_direction * direction * IMPULSE
+		
 		body.apply_central_impulse(velocity)
+		
 		if body.linear_velocity.length() > MAX_SPEED:
 			body.linear_velocity = body.linear_velocity.normalized() * MAX_SPEED
