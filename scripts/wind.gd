4188eba1838532a3321bd88ab33f93181b585101
Franco Martín Salgado Sierra <frco.indie@gmail.com>
2024-12-01 19:25:02 -0300

    Revert "Se añade y arregla el viento y el elefante"
    This reverts commit f5af6ce028a1a963a1b18f05a3787c6d65ad3922.

--- scripts/wind.gd
+++ scripts/wind.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var blow = $blow
 
 @export_subgroup("Forces")
 @export var FORCE_VECTOR: Vector2 = Vector2(8.0, 0.0)
@@ -15,17 +14,10 @@ var dung_in: bool
 var beetle_in: bool
 var rng = RandomNumberGenerator.new()
 var force_x: float
-var bettle_pulled: bool = false
-var dung_ball_pulled: bool = false
+
 
 func _physics_process(delta: float) -> void:
-	if animated_sprite_2d.is_playing():
-		if beetle_in and !bettle_pulled:
-			beetle.apply_central_impulse(-FORCE_VECTOR)
-			bettle_pulled = true
-		if dung_in and !dung_ball_pulled:
-			dung_ball.apply_central_impulse(-FORCE_VECTOR)
-			dung_ball_pulled = true
+	pass
 
 
 func _on_push_area_body_entered(body: Node2D) -> void:
@@ -42,10 +34,8 @@ func _on_push_area_body_exited(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	bettle_pulled = false
-	dung_ball_pulled = false
 	animated_sprite_2d.play("breeze")
-	blow.play()
-	blow.volume_db = 20.0
-	var tween = create_tween()
-	tween.tween_property(blow, "volume_db", -80.0, 3.2)
+	if beetle_in:
+		beetle.apply_central_impulse(-FORCE_VECTOR)
+	if dung_in:
+		dung_ball.apply_central_impulse(-FORCE_VECTOR)
